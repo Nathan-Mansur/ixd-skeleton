e1a492699cba52d6b745be589351e9131d515f31
Michelle Nguyen <mnn027@ucsd.edu>
2018-02-22 21:45:45 -0800

    fixed navbar for publicList

--- public/js/publicList.js
+++ public/js/publicList.js
@@ -10,3 +10,13 @@ function closeAdd() {
 	//document.getElementById("taskInput").placeholder = "add a task"; /* This isn't working */
 	document.getElementById("addPop").style.zIndex = "-1";
 }
+
+
+//side nav bar stuff
+function openNav() {
+	document.getElementById("mySidenav").style.width = "225px";
+}
+
+function closeNav() {
+	document.getElementById("mySidenav").style.width = "0";
+}
