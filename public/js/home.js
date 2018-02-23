f0e6408038f20a0c53e675f22fcad0d32dfdfbba
Nathaniel Mansur <njmansur@ucsd.edu>
2018-02-22 21:21:56 -0800

    Added date to home page

--- public/js/home.js
+++ public/js/home.js
@@ -14,6 +14,15 @@ $(document).ready(function() {
 	*/
 })
 
+// Loads date
+window.onload = function date(){
+	n = new Date();
+	y = n.getFullYear();
+	m = n.getMonth() + 1;
+	d = n.getDate();
+	document.getElementById("date").innerHTML = m + "/" + d + "/" + y;
+}
+
 
 function changeState(e) {
 	e.preventDefault();
