8daeb22007e13a321bc1a30c56eba163232d9eeb
Nathaniel Mansur <njmansur@ucsd.edu>
2018-02-22 22:54:26 -0800

    Time button added

--- public/js/home.js
+++ public/js/home.js
@@ -23,6 +23,19 @@ window.onload = function date(){
 	document.getElementById("date").innerHTML = m + "/" + d + "/" + y;
 }
 
+// Date chooser on Popup
+ $(document).ready(function(){
+      var date_input=$('input[name="date"]'); //our date input has the name "date"
+      var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+      var options={
+        format: 'mm/dd/yyyy',
+        container: container,
+        todayHighlight: true,
+        autoclose: true,
+      };
+      date_input.datepicker(options);
+    })
+
 
 function changeState(e) {
 	e.preventDefault();
--- routes/add.js
+++ routes/add.js
@@ -7,8 +7,10 @@ var data = require("../tasks.json");
 exports.addTask = function(req, res){
 
     var newTask = {
-        'name': req.query.name
+        'name': req.query.name,
+        'time': req.query.time
     }
+
 
     data.tasks.push(newTask);
     res.render('home', {
