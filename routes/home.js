fb51d72efd960120ab25ff478dd0eaf7a8096b6f
Lee Liang <ltliang1@gmail.com>
2018-02-24 16:09:13 -0800

    package-lock.json

--- routes/home.js
+++ routes/home.js
@@ -10,30 +10,30 @@ exports.view = function(req, res){
   });
 };
 
-// // Imports the Google Cloud client library
-// const language = require('@google-cloud/language');
+// Imports the Google Cloud client library
+const language = require('@google-cloud/language');
 
-// // Instantiates a client
-// const client = new language.LanguageServiceClient();
+// Instantiates a client
+const client = new language.LanguageServiceClient();
 
-// // The text to analyze
-// const text = 'Hello, world!';
+// The text to analyze
+const text = 'Hello, world!';
 
-// const document = {
-//   content: text,
-//   type: 'PLAIN_TEXT',
-// };
+const document = {
+  content: text,
+  type: 'PLAIN_TEXT',
+};
 
-// // Detects the sentiment of the text
-// client
-//   .analyzeSentiment({document: document})
-//   .then(results => {
-//     const sentiment = results[0].documentSentiment;
+// Detects the sentiment of the text
+client
+  .analyzeSentiment({document: document})
+  .then(results => {
+    const sentiment = results[0].documentSentiment;
 
-//     console.log(`Text: ${text}`);
-//     console.log(`Sentiment score: ${sentiment.score}`);
-//     console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
-//   })
-//   .catch(err => {
-//     console.error('ERROR:', err);
-//   });
+    console.log(`Text: ${text}`);
+    console.log(`Sentiment score: ${sentiment.score}`);
+    console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  });
