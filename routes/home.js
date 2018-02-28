515f4b657d62938bb8492d4a0575f1f89a304177
Lee Liang <ltliang1@gmail.com>
2018-02-27 16:13:33 -0800

    aws commit

--- routes/home.js
+++ routes/home.js
@@ -51,26 +51,26 @@ exports.view = function(req, res){
 //     console.error('ERROR:', err);
 //   });
 
-// // aws comprehend
-// var AWS = require('aws-sdk');
+// aws comprehend
+var AWS = require('aws-sdk');
 
-// AWS.config.apiVersions = {
-//   comprehend: '2017-11-27',
-//   // other service API versions
-// };
+AWS.config.apiVersions = {
+  comprehend: '2017-11-27',
+  // other service API versions
+};
 
-// AWS.config.update({region:'us-west-2'});
+AWS.config.loadFromPath('./config.json');
 
-// var comprehend = new AWS.Comprehend();
+var comprehend = new AWS.Comprehend();
 
-// var params = {
-//   TextList: [ /* required */
-//     '​Start CSE100 PA2 by Tomorrow at 8am',
-//     /* more text */
-//   ]
-// };
+var params = {
+  TextList: [ /* required */
+    '​Start CSE100 PA2 by Tomorrow at 8am',
+    /* more text */
+  ]
+};
 
-// comprehend.batchDetectDominantLanguage(params, function (err, data) {
-//   if (err) console.log(err, err.stack);             // an error occurred
-//   else     console.log(data.ResultList[0]);       // successful response
-// });
+comprehend.batchDetectDominantLanguage(params, function (err, data) {
+  if (err) console.log(err, err.stack);             // an error occurred
+  else     console.log(data.ResultList[0]);       // successful response
+});
