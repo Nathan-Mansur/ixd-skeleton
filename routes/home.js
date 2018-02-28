f7d78a49f539018fccc75ab7535a312873a75bb9
Lee Liang <ltliang1@gmail.com>
2018-02-27 23:15:39 -0800

    home.js routes fix

--- routes/home.js
+++ routes/home.js
@@ -10,7 +10,7 @@ exports.view = function(req, res){
 
   var currentUser = "123456abcdef";
   var pendingData = [];
-  for (i = 0; i <  data.tasks.size(); i++) {
+  for (i = 0; i <  data.tasks.length; i++) {
       if (data.tasks[i].userID == currentUser ){
           pendingData.push(data.tasks[i]);
       }
