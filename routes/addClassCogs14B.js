985938233104721d7129d6e12fc178c3525a6049
Nathaniel Mansur <njmansur@ucsd.edu>
2018-03-01 16:44:45 -0800

    Wizard of Oz Add a Class

--- routes/addClassCogs14B.js
+++ routes/addClassCogs14B.js
@@ -3,8 +3,11 @@
  */
 
 var data = require("../tasks.json");
-var classData = require("../cogs120tasks.json");
+var classData = require("../cogs14Btasks.json");
 
 exports.addClass = function(req, res){
+	for (i = 0; i < classData.tasks.length; i++) {
+		data.tasks.push(classData.tasks[i]);
+	}
   	res.render('addClass');
 };
