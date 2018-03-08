cc560131a630d2f7de73ee8b8bf800cba803c746
Lee Liang <ltliang1@gmail.com>
2018-03-08 01:22:47 -0800

    add routes/homeAlt.js

--- routes/home.js
+++ routes/home.js
@@ -21,25 +21,6 @@ exports.view = function(req, res){
   });
 };
 
-var data = require("../tasksAlt.json");
-
-exports.viewAlt = function(req, res){
-
-  // checking user
-
-  var currentUser = "123456abcdef";
-  var pendingData = [];
-  for (i = 0; i < data.tasks.length; i++) {
-      if (data.tasks[i].userID == currentUser) {
-          pendingData.push(data.tasks[i]);
-      }
-  }
-
-  res.render('homeAlt', {
-      'tasks': pendingData
-  });
-};
-
 // // Imports the Google Cloud client library
 // const language = require('@google-cloud/language');
 
---
+++ routes/homeAlt.js
@@ -0,0 +1,18 @@
+var data = require("../tasksAlt.json");
+
+exports.viewAlt = function(req, res){
+
+  // checking user
+
+  var currentUser = "123456abcdef";
+  var pendingData = [];
+  for (i = 0; i < data.tasks.length; i++) {
+      if (data.tasks[i].userID == currentUser) {
+          pendingData.push(data.tasks[i]);
+      }
+  }
+
+  res.render('homeAlt', {
+      'tasks': pendingData
+  });
+};
